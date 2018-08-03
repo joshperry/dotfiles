a43e8028ec13227a9f6e37f15501b04700a78647
Joshua Perry <josh@6bit.com>
2018-08-03 11:05:49 -0600

    Add i3 config

---
+++ .config/i3/scripts/screen-office.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output eDP-1 --off --output DP-1-2 --primary --mode 2560x1600 --pos 0x880 --rotate normal --output HDMI-2 --off --output HDMI-1 --off --output DP-1 --off --output DP-1-3 --off --output DP-2 --off --output DP-1-1 --mode 2560x1440 --pos 2560x0 --rotate left
