b60685beff0be49908ca7d494b2e590b240019c4
Joshua Perry <josh@6bit.com>
2018-09-19 09:16:06 -0600

    Update screen setups for new monitor

--- .config/i3/scripts/screen-office.sh
+++ .config/i3/scripts/screen-office.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output eDP-1 --off --output DP-1-2 --primary --mode 2560x1600 --pos 0x880 --rotate normal --output HDMI-2 --off --output HDMI-1 --off --output DP-1 --off --output DP-1-3 --off --output DP-2 --off --output DP-1-1 --mode 2560x1440 --pos 2560x0 --rotate left
+xrandr --output DP-2-1 --off --output DP-1-2 --primary --mode 3840x1600 --pos 0x872 --rotate normal --output DP-2-3 --off --output eDP-1 --off --output DP-1-2 --off --output HDMI-2 --off --output HDMI-1 --off --output DP-1 --off --output DP-1-3 --off --output DP-2 --off --output DP-2-1 --mode 2560x1440 --pos 3840x0 --rotate left
